0661c1e923c06b7a08a4304065ad1b108f0dee0c
Lokesh <lokeshsingh7695@gmail.com>
2023-04-12 18:27:42 +0530

    Day 1

---
+++ Syntax.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h> // Header file of the program
+using namespace std; // Namespace is used to overcome name conflicts and it also provide direct access of cin,cout functions
+
+int main()// main function considered as the entry gate of the program where the program starts executing
+{
+
+return 0;// return 0 to the main function as the main function has a return type of int and it aslo indicate the end of the program.
+}
