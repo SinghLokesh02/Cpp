09f385d9ef7a4f3f79134cdf353674e7c3ee79bc
Lokesh <lokeshsingh7695@gmail.com>
2023-04-26 13:58:47 +0530

    Update the code with desc

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+/*
+#include is preprocessor directive which tells the compiler to include the code from specified file like we have "#include<iostream>" means include the code from the iostream file like all the functions etc.
+*/
 using namespace std;
+/*
+In C++, the "using" keyword is used to declare a using declaration or a using directive, which allows you to use names from a namespace without having to qualify them with the namespace name every time.
 
-int main(){
-    cout<<"Lokesh Singh";
+The "std" namespace in C++ contains a lot of useful functions and classes for performing input/output operations, manipulating strings, and more. In order to use these functions and classes in your C++ program, you need to either explicitly qualify them with the "std::" prefix, or you can use the "using namespace std;" directive to tell the compiler to use the "std" namespace for all names that are not otherwise declared.
+
+For example, if you want to use the "cout" and "endl" functions from the "std" namespace in your C++ program, you would normally have to use them like this:
+*/
+int main()
+{
+    /*
+    In C++, the "main" function is a special function that serves as the entry point for the program. When you run a C++ program, the operating system calls the "main" function to start executing the program.
+
+    The program code that you write in the "main" function typically consists of a sequence of statements that perform some computation or I/O operation. When the "main" function finishes executing, it should return an integer value to indicate whether the program completed successfully or not. By convention, a return value of 0 indicates successful completion, and a non-zero value indicates an error.
+    */
+    cout << "Lokesh Singh";
     return 0;
 }
-
- 
