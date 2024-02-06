48b135461b1a944a5beeef1737140fd1d5caff5b
Lokesh <lokeshsingh7695@gmail.com>
2024-02-06 10:47:50 +0530

    Fixed the bugs now

--- Avoid_Classname_conflicts/File2.cpp
+++ Avoid_Classname_conflicts/File2.cpp
@@ -1,21 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-class Student
+namespace File2
 {
-public:
-    string name;
-    int age;
-    void Set_data(string name, int age)
-    {
-        this->name = name;
-        this->age = age;
-    }
 
-    void display()
+    class Student
     {
-        cout << "Name: " << name << "\nAge: " << age << endl;
-    }
-};
+    public:
+        string name;
+        int age;
+        void Set_data(string name, int age)
+        {
+            this->name = name;
+            this->age = age;
+        }
 
- 
+        void display()
+        {
+            cout << "Name: " << name << "\nAge: " << age << endl;
+        }
+    };
+}
--- Avoid_Classname_conflicts/main.cpp
+++ Avoid_Classname_conflicts/main.cpp
@@ -1,18 +1,19 @@
 #include"File1.cpp"
 #include"File2.cpp"
+#include <bits/stdc++.h>
 
 
 int main()
 {
     // Creating object of Student class from File1
     cout << "Student 1 Details: " << endl;
-    Student s1;
+    File1::Student s1;
     s1.Set_data1("John", 25,34);
     s1.display();
 
     // Creating object of Student class from File2
     cout << "\nStudent 2 Details: " << endl;
-    Student s2;
+    File2::Student s2;
     s2.Set_data("Ravi", 30);
     s2.display();
 
